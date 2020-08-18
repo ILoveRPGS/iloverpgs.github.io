43e576cfff2346bc3637d28144675ca32ef56852
ILoveRPGS <mitja.savijarvi@edu.tampere.fi>
2020-08-18 09:52:25 +0300

    18.8 Harjoitus

---
+++ jscode.js
@@ -0,0 +1,9 @@
+let mina = prompt ("Anna nimesi: ")
+
+if (mina == "Mitja"){
+     console.log("Welcome Onii-chan")
+}
+else if (mina == "Sardor"){ 
+     console.log("What are u doing here u bastard!!")
+}
+else console.log("Youre not welcome");
